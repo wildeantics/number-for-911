3a0ac7b410d5e0c4bb56b47ef33552e7a918ee0c
Morgan Wilde <morgan@wilde.mx>
2022-05-19 15:47:39 +1000

    Attempt to get api to work against cors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import CountryList from './assets/cc.json'
 
 function App() {
+  const [emergencyNumber, setEmergencyNumber] = useState()
+  const [country, setCountry] = useState('')
+  const [countryCode, setCountryCode] = useState()
+
+  const handleChange = (e) => {
+    setCountryCode(e.target.value)
+    getEmergencyNumber()
+  }
+
+  const getGeoInfo = async () => {
+    axios
+      .get('https://ipapi.co/json/')
+      .then((response) => {
+        setCountry(response.data.country_name)
+        setCountryCode(response.data.country_code)
+        getEmergencyNumber()
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  const getEmergencyNumber = async () => {
+    axios
+      .get(`https://emergencynumberapi.com/api/country/${countryCode}`)
+      .then((response) => {
+        setEmergencyNumber(response.data.fixed)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getGeoInfo()
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main className='h-screen w-screen'>
+      <h1>Your number for 911 is</h1>
+      <h2>{emergencyNumber}</h2>
+      <label htmlFor='country'>Actually I live in...</label>
+      <select name='country' id='country' onChange={handleChange}>
+        <option disabled>one of these countries</option>
+        {CountryList.map((country) => (
+          <option key={country.alpha2} value={country.alpha2}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+      <footer>
+        <p className='strong'>
+          If you are in trouble just call 911, it will redirect to the local
+          emergency services
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          The data from this website is provided without any claims of accuracy,
+          you should use this data as guidance, and do your own due diligence.
+        </p>
+        <p className='footer'>Made by Morgan Wilde with React</p>
+        {/* Social links */}
+      </footer>
+      <div className='background'>{country}</div>
+    </main>
+  )
 }
 
-export default App;
+export default App
