387684f95b611c80810783d25681a77553b97ce7
Morgan Wilde <morgan@wilde.mx>
2022-05-27 11:34:21 +1000

    Added MUI dark theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { createTheme, CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/system'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
